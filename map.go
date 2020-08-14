7c5559335ecaece1df3f4056567d6a3cb1dd1494
Sataapon <mr.sataapon@gmail.com>
2020-08-15 00:07:30 +0700

    Remove symbols

--- map.go
+++ map.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
 	"strings"
 )
 
@@ -14,8 +16,16 @@ func WordCounts(s string) map[string]int {
 	return res
 }
 
+func RemoveSymbols(s string) string {
+	reg, err := regexp.Compile(`[^\w]`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return reg.ReplaceAllString(s, " ")
+}
+
 func main() {
 	s := "If it looks like a duck, swims like a duck, and quacks like a duck, then it probably is a duck."
-	w := WordCounts(s)
+	w := WordCounts(RemoveSymbols(s))
 	fmt.Println(w)
 }
